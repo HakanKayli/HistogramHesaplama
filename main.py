70126c9db8a416840eade71ba6e90624a55fd243
Senanur Paksoy <66176697+senanurpaksoy@users.noreply.github.com>
2022-12-21 14:32:28 +0300

    görüntünün tersini alma fonksiyonu eklendi

--- main.py
+++ main.py
@@ -17,4 +17,37 @@ plt.plot(Hist)
 plt.show()
 
 cv2.imshow("Kaptan Amerika", foto)
+
+
+
+h = 300
+w = 400
+
+original = cv2.imread("kaptanamerika.jpeg",0)
+original = cv2.resize(original,(w,h))
+
+image = cv2.imread("kaptanamerika.jpeg",0)
+image = cv2.resize(image,(w,h))
+
+[h,w] = shape(image)
+image2 = zeros([h,w], dtype=np.uint8)
+
+max = 0
+
+#maximumu bulma döngüsü
+for i in range (h):
+    for j in range (w):
+        if max < image [i,j]:
+            max = image [i,j]
+
+
+#maximumdan görüntü matrisinin her değerini çıkarma döngüsü
+for i in range (h):
+    for j in range (w):
+        image2[i,j] = max - image[i,j]
+
+
+cv2.imshow("original", original)
+cv2.imshow("gri",image)
+cv2.imshow("ters cevrilmis",image2)
 cv2.waitKey()
